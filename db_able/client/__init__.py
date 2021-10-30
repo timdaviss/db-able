b4b61eb1ee6f07c0ec42b7f15649c29706eec564
Tim Davis <timdavis.3991@gmail.com>
2021-10-30 15:04:46 -0500

    Remove dbconfig

--- db_able/client/__init__.py
+++ db_able/client/__init__.py
@@ -1,3 +1,21 @@
 """
 :date_created: 2021-10-23
 """
+
+from do_py import DataObject, R
+
+
+class DBClient(DataObject):
+    _restrictions = {
+        'url': R.STR,
+        'username': R.STR,
+        'password': R.STR
+        }
+
+    def __enter__(self):
+        """"""
+        # Open connection
+        # Execute command
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        """"""
