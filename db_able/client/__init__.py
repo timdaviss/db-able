072b908ac013a25fe1350f2b0203fa2bd9a889ce
Tim Davis <timdavis.3991@gmail.com>
2021-10-30 15:37:24 -0500

    wip

--- db_able/client/__init__.py
+++ db_able/client/__init__.py
@@ -2,15 +2,33 @@
 :date_created: 2021-10-23
 """
 
-from do_py import DataObject, R
+from do_py.utils import cached_property
 
 
-class DBClient(DataObject):
-    _restrictions = {
-        'url': R.STR,
-        'username': R.STR,
-        'password': R.STR
-        }
+class DBClient(object):
+
+    def __init__(self, database, stored_procedure, *args, **kwargs):
+        """"""
+        self.database = database
+        self.stored_procedure = stored_procedure
+        self.args = args
+        self.kwargs = kwargs
+
+    @cached_property
+    def sql(self):
+        """
+        
+        :rtype: str
+        """
+        return '`{database}`.`{stored_procedure}`({args});'.format(
+            database=self.database,
+            stored_procedure=self.stored_procedure,
+            args=','.join(self.args)
+            )
+
+    def open_connection(self):
+        """"""
+
 
     def __enter__(self):
         """"""
